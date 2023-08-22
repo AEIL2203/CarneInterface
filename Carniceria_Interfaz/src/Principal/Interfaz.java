c62f5cf39e4aa35edfaa963c4dc959202a859b31
angel <angel@Angel>
2023-08-21 21:02:22 -0600

    Parte 2

--- Carniceria_Interfaz/src/Principal/Interfaz.java
+++ Carniceria_Interfaz/src/Principal/Interfaz.java
@@ -1,16 +1,6 @@
 package Principal;
 
 public interface Interfaz {
-	public static void main(String[] args) {
-		  Producto carneRes = new Producto("Carne de Res", 10.5, 15);
-	        Producto pollo = new Producto("Pollo", 7.2, 30);
-	        Producto chorizo = new Producto("Chorizo", 5.0, 25);
-
-	        Factura factura = new Factura();
-	        factura.agregarProducto(carneRes);
-	        factura.agregarProducto(pollo);
-	        factura.agregarProducto(chorizo);
-
-	        factura.imprimirFactura();
-	}
+	public void Factura();
+	public void Producto();
 }
